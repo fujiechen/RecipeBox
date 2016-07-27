a02fc82fce9d664a005df8053f14fe44a98d8937
Fujie Chen <imaple7@gmail.com>
2016-07-27 18:07:02 -0500

    Update Main.java

--- app/src/main/java/comp3350/recipebox/application/Main.java
+++ app/src/main/java/comp3350/recipebox/application/Main.java
@@ -5,6 +5,7 @@ public class Main
     public static final String dbName="RB";
     private static String dbPathName = "database/RB";
 
+    //Start the app
     public static void main(String[] args)
     {
         startUp();
@@ -14,16 +15,19 @@ public class Main
         System.out.println("All done");
     }
 
+    //Create Data Access
     public static void startUp()
     {
         Services.createDataAccess(dbName);
     }
 
+    //Close Data Access
     public static void shutDown()
     {
         Services.closeDataAccess();
     }
 
+    //Get the Database Path Name
     public static String getDBPathName()
     {
         if(dbPathName == null)
@@ -32,6 +36,7 @@ public class Main
             return dbPathName;
     }
 
+    //Set the Database Path Name
     public static void setDBPathName(String pathName)
     {
         System.out.println("Setting DB path to: " + pathName);
