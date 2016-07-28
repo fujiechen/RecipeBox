51c9384b468e227f15316128a1ee275aab160b5b
Fujie Chen <imaple7@gmail.com>
2016-07-27 22:21:29 -0500

    Update AccessRecipe.java

--- app/src/main/java/comp3350/recipebox/business/AccessRecipe.java
+++ app/src/main/java/comp3350/recipebox/business/AccessRecipe.java
@@ -13,18 +13,21 @@ public class AccessRecipe
 	private DataAccess dataAccess;
 	private ArrayList<Recipe> recipes;
 
+	//Constructor
 	public AccessRecipe()
 	{
 		dataAccess = Services.getDataAccess(Main.dbName);
 		recipes = null;
 	}
 
+	//Constructor
 	public AccessRecipe(DataAccess dataAccess)
 	{
 		this.dataAccess = dataAccess;
 		this.dataAccess = Services.getDataAccess(Main.dbName);
 	}
 
+	//Add new recipe
 	public void addRecipe(String title, ArrayList<String> ingredients, String instruction, User user)
 	{
 		if (user == null || title == null)
@@ -37,6 +40,7 @@ public class AccessRecipe
 		dataAccess.addNewRecipe(title, ingredients, instruction, user);
 	}
 
+	//Get the whole recipe list
 	public ArrayList<Recipe> getRecipes()
 	{
 		recipes = new ArrayList<Recipe>();
@@ -45,6 +49,7 @@ public class AccessRecipe
 		return recipes;
 	}
 
+	//Get recipes by the given keyword
 	public ArrayList<Recipe> getRecipesByKeyword(String keyword)
 	{
 
@@ -54,21 +59,25 @@ public class AccessRecipe
 		return recipes;
 	}
 
+	//Delete recipe by its id
 	public void deleteRecipe(int id)
 	{
 		dataAccess.deleteRecipeByID(id);
 	}
 
+	//Update recipe by its id
 	public void updateRecipe(int id, String title, ArrayList<String> ingredients, String instructions)
 	{
 		dataAccess.updateRecipe(id, title, ingredients, instructions);
 	}
 
+	//Get recipe by its id
 	public Recipe getRecipe(int id)
 	{
 		return dataAccess.findRecipeByID(id);
 	}
 
+	//Get recipe list by the user object
 	public ArrayList<Recipe> getRecipeListByUser(User user)
 	{
 		recipes = new ArrayList<Recipe>();
